04ea2eee2ba24f3ef4da128ad205dd0dd7eed4eb
caferven <carol96lorac@gmail.com>
2024-09-16 21:10:31 +0200

    dark theme & responsive tweaks

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path("i18n/", include("django.conf.urls.i18n")),
 )
 
 if settings.DEBUG:
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,19 @@
+const body = document.querySelector("body")
 const hamburgerMenu = document.querySelector("#hamburger-menu")
 const dropdownMenu = document.querySelector("#dropdown-menu")
+const themeMode = document.querySelector("#theme-mode")
 
 hamburgerMenu.addEventListener("click", ()=>{
     dropdownMenu.classList.toggle("hidden")
+})
+
+themeMode.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
+    if (body.classList.contains("dark")) {
+        themeMode.src = lightModeIcon
+        themeMode.alt = "Light mode"
+    } else {
+        themeMode.src = darkModeIcon
+        themeMode.alt = "Dark mode"
+    }
 })
