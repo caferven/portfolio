b63e7194d4da88cbd943b1dd781562bd5c1c590f
caferven <carol96lorac@gmail.com>
2024-09-18 16:43:41 +0200

    header changes

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,16 @@ const body = document.querySelector("body")
 const hamburgerMenu = document.querySelector("#hamburger-menu")
 const dropdownMenu = document.querySelector("#dropdown-menu")
 const themeMode = document.querySelector("#theme-mode")
+const menuLinks = document.querySelectorAll("#dropdown-menu a")
 
 hamburgerMenu.addEventListener("click", ()=>{
     dropdownMenu.classList.toggle("hidden")
+})
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        dropdownMenu.classList.add("hidden")
+    })
 })
 
 themeMode.addEventListener("click", ()=>{
